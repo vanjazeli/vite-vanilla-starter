703c1ce8ed8d1d2f65f92ec75ff4c94fa9226dac
vanjazeli <vanjazeli@gmail.com>
2023-03-10 08:38:59 +0100

    feat: add babel on build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import fs from 'fs-extra';
+import babel from 'rollup-plugin-babel';
 
 export default defineConfig({
   root: './src',
@@ -13,6 +14,12 @@ export default defineConfig({
         chunkFileNames: 'js/[name].js',
         assetFileNames: 'styles/[name].[ext]',
       },
+      plugins: [
+        babel({
+          exclude: ['node_modules/**','src/**'],
+          presets: ['@babel/preset-env']
+        })
+      ],
       manualChunks(id) {
         if(id.endsWith("index.html")) {
           fs.copySync("src/assets", './dist/assets');
